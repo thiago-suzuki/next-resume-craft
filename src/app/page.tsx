250330bbe4d18bbf50319efd60501684f0c9d6c2
Thiago Suzuki <thiagoikenaga@gmail.com>
2025-06-29 16:06:52 -0300

    feat: add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,47 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Logo from "@/assets/logo.svg";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  description: "Crie currículos de forma rápida e fácil com nossos modelos gratuitos.",
+}
 
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="font-title">Welcome to ResumeCraft</h1>
+    <main className="w-full h-screen bg-gradient-to-tl from-muted flex flex-col md:justify-center p-6 overflow-x-hidden">
+      <div className="w-full max-w-[1200px] mx-auto flex flex-col md:flex-row gap-8 md:gap-12">
+        <div>
+          <Logo className="w-full max-w-[100px] mb-8" />
+
+          <h1 className="font-title font-bold text-5xl max-w-[500px]">
+            Um criador de currículos gratuito e fácil de usar
+          </h1>
+          <p className="text-muted-foreground text-lg mt-2">
+            Comece a criar seus currículos de forma rápida e fácil com nossos
+            modelos.
+          </p>
+
+          <Link href="/dashboard/resumes" passHref>
+            <Button className="mt-4">Começar agora</Button>
+          </Link>
+        </div>
+
+        <div className="flex-1 relative h-full">
+          <Image
+            src="/images/dashboard.webp"
+            alt="Imagem do painel do ResumeCraft"
+            width={1200}
+            height={800}
+            className={cn(
+              "md:absolute md:top-1/2 md:-translate-y-1/2 md:left-0 md:min-w-[80vw]",
+              "rounded-lg overflow-hidden border-2 border-muted"
+            )}
+          />
+        </div>
+      </div>
     </main>
   );
 }
