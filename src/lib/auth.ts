4e4e4d0c22a5b1dc3932a0d25918598554b0e5f5
Thiago Suzuki <thiagoikenaga@gmail.com>
2025-06-28 22:19:16 -0300

    feat: add connection db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,19 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db/drizzle";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [GitHub, Google],
-    pages: {
-        newUser: "/",
-        signIn: "/auth/login",
+  adapter: DrizzleAdapter(db),
+  providers: [GitHub, Google],
+  pages: {
+    signIn: "/auth/login",
+  },
+  callbacks: {
+    authorized: async ({ auth }) => {
+      return !!auth;
     },
-    callbacks: {
-        authorized: async ({ auth }) => {
-            return !!auth;
-        },
-    },
+  },
 });
