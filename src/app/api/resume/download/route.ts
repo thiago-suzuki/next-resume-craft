76739b131121c19c22daa426429bd38a3edc0248
Thiago Suzuki <thiagoikenaga@gmail.com>
2025-06-29 20:56:30 -0300

    test: add log

--- src/app/api/resume/download/route.ts
+++ src/app/api/resume/download/route.ts
@@ -51,6 +51,7 @@ export const POST = async (request: Request) => {
       }
     })
   } catch (error) {
+    console.log("Meu erro", error)
     console.error(error);
     return Response.json(
       { message: "Ocorreu um erro inesperado", error },
